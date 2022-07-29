51f88baa3133c654f639a9a9998f35f60cf4bbfd
Evgeny Vasiltsov <evgeny.vasiltsov@mail.ru>
2022-07-29 16:23:35 +0300

    Добавлены скрипты для переключения tabs

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,11 +1,15 @@
-(document).ready(function(){
+$(document).ready(function(){
 
+  $('.js-tabs-link').on('click', function(){
+    $('.js-tabs-link').removeClass('active');
+    $(this).addClass('active');
 
+    let index = $(this).index('.js-tabs-link');
 
+    $('.js-tabs-content').removeClass('active');
+    $('.js-tabs-content').eq(index).addClass('active');
 
-
-
-
+  })
 
 
 })
